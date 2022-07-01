90be061ed970e018c201389423bfc0daaf04253b
yonilurie <yonilurie@protonmail.com>
2022-07-01 11:57:34 -0700

    Readded previously dleetd code to set cookie

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,11 +2,18 @@ const router = require("express").Router();
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
+
+const { requireAuth } = require("../../utils/auth.js");
+router.get("/require-auth", requireAuth, (req, res) => {
+	return res.json(req.user);
+});
 
 router.use("/session", sessionRouter);
 
@@ -14,6 +21,20 @@ router.use("/users", usersRouter);
 
 router.post("/test", (req, res) => {
 	res.json({ requestBody: req.body });
+});
+
+router.get("/set-token-cookie", async (_req, res) => {
+	const user = await User.findOne({
+		where: {
+			username: "Demo-lition",
+		},
+	});
+	setTokenCookie(res, user);
+	return res.json({ user });
+});
+
+router.get("/restore-user", (req, res) => {
+	return res.json(req.user);
 });
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,7 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
+
 router.get("/api/csrf/restore", (req, res) => {
 	const csrfToken = req.csrfToken();
 	res.cookie("XSRF-TOKEN", csrfToken);
@@ -12,5 +13,7 @@ router.get("/api/csrf/restore", (req, res) => {
 		"XSRF-Token": csrfToken,
 	});
 });
+
+
 
 module.exports = router;
