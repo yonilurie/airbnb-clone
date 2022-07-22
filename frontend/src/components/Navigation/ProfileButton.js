45dc080c9a704e3563e7a9a3deeef4cca32db014
yonilurie <yonilurie@protonmail.com>
2022-07-22 10:27:18 -0700

    Added basic styling to navigation

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,49 +1,73 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+import "./ProfileButton.css";
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
-  
-  useEffect(() => {
-    if (!showMenu) return;
+	const dispatch = useDispatch();
+	const [showMenu, setShowMenu] = useState(false);
 
-    const closeMenu = () => {
-      setShowMenu(false);
-    };
+	const openMenu = () => {
+		if (showMenu) return;
+		setShowMenu(true);
+	};
 
-    document.addEventListener('click', closeMenu);
-  
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+	useEffect(() => {
+		if (!showMenu) return;
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+		const closeMenu = () => {
+			setShowMenu(false);
+		};
 
-  return (
-    <>
-      <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
-      </button>
-      {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )}
-    </>
-  );
+		document.addEventListener("click", closeMenu);
+
+		return () => document.removeEventListener("click", closeMenu);
+	}, [showMenu]);
+
+	const logout = (e) => {
+		e.preventDefault();
+		dispatch(sessionActions.logout());
+	};
+
+	return (
+		<>
+			<button onClick={openMenu}>
+				<i className="fas fa-user-circle" />
+				<svg
+					viewBox="0 0 32 32"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true"
+					role="presentation"
+					focusable="false"
+					style={{
+						display: "block",
+						fill: "none",
+						height: "16px",
+						width: "16px",
+						stroke: "currentcolor",
+						strokeWidth: "3",
+						overflow: "visible",
+					}}
+				>
+					<g fill="none" fill-rule="nonzero">
+						<path d="m2 16h28"></path>
+						<path d="m2 24h28"></path>
+						<path d="m2 8h28"></path>
+					</g>
+				</svg>
+			</button>
+
+			{showMenu && (
+				<ul className="profile-dropdown">
+					<li>{user.username}</li>
+					<li>{user.email}</li>
+					<li>
+						<button onClick={logout}>Log Out</button>
+					</li>
+				</ul>
+			)}
+		</>
+	);
 }
 
 export default ProfileButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import logo from "../../images/logo.svg";
 
 function Navigation({ isLoaded }) {
 	const sessionuser = useSelector((state) => state.session.user);
@@ -13,19 +14,23 @@ function Navigation({ isLoaded }) {
 	} else {
 		sessionLinks = (
 			<>
-				<NavLink to="/login">Log In</NavLink>
-				<NavLink to="/signup">Sign Up</NavLink>
+				<NavLink to="/login" className="nav-link">
+					Log In
+				</NavLink>
+				<NavLink to="/signup" className="nav-link">
+					Sign Up
+				</NavLink>
 			</>
 		);
 	}
 	return (
 		<ul>
 			<li>
-				<NavLink exact to="/">
-					Home
+				<NavLink exact to="/" className="nav-link">
+					<img src={logo} alt="logo"></img>
 				</NavLink>
-				{isLoaded && sessionLinks}
 			</li>
+			<li>{isLoaded && sessionLinks}</li>
 		</ul>
 	);
 }
