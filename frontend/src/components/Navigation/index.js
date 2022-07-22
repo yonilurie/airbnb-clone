c890ca766c774893efbb22a8be1d177db2d869bf
yonilurie <yonilurie@protonmail.com>
2022-07-22 09:24:15 -0700

    Added navigation page

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+
+function Navigation({ isLoaded }) {
+	const sessionuser = useSelector((state) => state.session.user);
+
+	let sessionLinks;
+	if (sessionuser) {
+		sessionLinks = <ProfileButton user={sessionuser} />;
+	} else {
+		sessionLinks = (
+			<>
+				<NavLink to="/login">Log In</NavLink>
+				<NavLink to="/signup">Sign Up</NavLink>
+			</>
+		);
+	}
+	return (
+		<ul>
+			<li>
+				<NavLink exact to="/">
+					Home
+				</NavLink>
+				{isLoaded && sessionLinks}
+			</li>
+		</ul>
+	);
+}
+
+export default Navigation;
