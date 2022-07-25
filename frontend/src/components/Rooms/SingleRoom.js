d386403668b6f72cc55af222861dca99d24c44e3
yonilurie <yonilurie@protonmail.com>
2022-07-24 21:40:11 -0700

    Styled room cards

--- frontend/src/components/Rooms/SingleRoom.js
+++ frontend/src/components/Rooms/SingleRoom.js
@@ -7,14 +7,21 @@ function SingleRoom({ room }) {
 		console.log(room.id);
 	};
 	return (
-		<div className="room" onClick={(e) => showRoomDetails(e)} >
+		<div className="room" onClick={(e) => showRoomDetails(e)}>
+			<div className="favorite">♥️</div>
 			<img
 				src={room.previewImage}
 				alt={`image for room ${room.id}`}
 			></img>
-			<div>{room.name}</div>
-			<div>{room.price}</div>
-			<div>{room.name}</div>
+			<div className="room-info">
+				<div className="room-detail name">{room.name}</div>
+				<div className="room-detail star">Star 5.0</div>
+				<div className="room-detail availability">{"availability"}</div>
+				<div className="room-detail price">
+					<b style={{ color: "rgb(34, 34, 34)" }}>${room.price}</b>
+					night
+				</div>
+			</div>
 		</div>
 	);
 }
