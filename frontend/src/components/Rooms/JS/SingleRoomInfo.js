242462e18d3f88142d1d90e9b4afeb279665a4f7
yonilurie <yonilurie@protonmail.com>
2022-07-28 23:30:59 -0700

    Added loading screen for rooms

--- frontend/src/components/Rooms/JS/SingleRoomInfo.js
+++ frontend/src/components/Rooms/JS/SingleRoomInfo.js
@@ -29,7 +29,7 @@ const SingleRoomInfo = () => {
 			setIsDisplayed(true);
 		}, 300);
 
-		return () => clearTimeout(timeout)
+		return () => clearTimeout(timeout);
 	}, []);
 
 	//Assign room, images, and reviews to variables for easier access
@@ -78,6 +78,37 @@ const SingleRoomInfo = () => {
 
 	return (
 		<div className="content-container">
+			{!isDisplayed && (
+				<div className="placeholder-container">
+					<div className="placeholder-loading-section-1">
+						<div className="loading-strip-1"></div>
+						<div className="loading-strip-2"></div>
+					</div>
+					<div className="placeholder-loading-section-2">
+						<div className="placeholder-loading-box-1-container">
+							<div className="loading-box-1"></div>
+						</div>
+						<div className="placeholder-loading-box-2-container">
+							<div className="loading-box-2"></div>
+							<div
+								className="loading-box-2"
+								style={{ borderTopRightRadius: "12px" }}
+							></div>
+							<div className="loading-box-2"></div>
+							<div
+								className="loading-box-2"
+								style={{ borderBottomRightRadius: "12px" }}
+							></div>
+						</div>
+					</div>
+
+					<div className="placeholder-loading-section-3">
+						<div className="loading-strip-3"></div>
+						<div className="loading-strip-4"></div>
+					</div>
+				</div>
+			)}
+
 			{isDisplayed && currentRoom && (
 				<div className="room-content">
 					<div>
